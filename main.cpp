3d08483113b11526b93f7e9e2f50e91cd004f2b6
Dylan Windebank <dylanwindebank@my.unt.edu>
2022-12-13 21:07:26 -0600

    added comments for table initialization

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+//prints in format whatever table (NFA/DFA) is passed to it, with a vector of states also passed
 void PrintTable(vector<vector<vector<string>>> table, vector<string> states){
         cout << "States\t\t|0\t|1\n";
         cout << "--------------------------------\n";
@@ -20,14 +21,17 @@ void PrintTable(vector<vector<vector<string>>> table, vector<string> states){
 
 
 int main(){
+	//declarations of variables
 	int i, j, k, stateN, outputStateN;
     string state, outputState;
     vector<vector<vector<string>>> table;
     vector<string> states;
 
+	//enter states present on left side of NFA table
     cout << "Enter the total number of states of the NFA: ";
     cin >> stateN;
 
+	//for loop for state rows, but states are not inputed in table but they are inputed in the states vector
     for(i = 0; i < stateN; i++){
 		cout << endl;
 		cout << "Enter state " << i+1 << "'s letter (one uppercase letter): ";
@@ -35,6 +39,7 @@ int main(){
 		states.push_back(state);
 		vector<vector<string>> temp1;
 
+		//for loop for inputs (0 and 1) of table
 		for(j = 0; j < 2; j++){
 			vector<string> temp2;
 			cout << "Enter the # of output states for input "<< j << " of state " << state << ": ";
@@ -44,6 +49,8 @@ int main(){
 			if(outputStateN == 0){
 					outputStateN+=1;
 			}
+
+			//for loop for the output state(s) of inputs
 			for(k=0;k<outputStateN;k++){
 				cin >> outputState;
 				temp2.push_back(outputState);
@@ -52,10 +59,10 @@ int main(){
 		}
 		table.push_back(temp1);
 	}
+
+	//nfa table printing
 	cout << "\n\nNFA Table\n";
 	PrintTable(table, states);
 
-
-	
 	return 0;
 }
